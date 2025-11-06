0dcc42642193b0c94bc66b9c76aff64653346c95
Lattinone <ferraina.1857726@studenti.uniroma1.com>
2025-11-06 15:32:04 +0100

    now it works

---
+++ build/ros_2d_multi_robot_simulator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lattinone/RobotProgramming/src/ros_2d_multi_robot_simulator
+# Build directory: /home/lattinone/RobotProgramming/build/ros_2d_multi_robot_simulator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
